500fe88ffa1ede8c9d39751d95e93a4408655f55
Nadav Lahish <d_lahish@MacBook-Air-2.home>
2016-06-22 22:33:06 +0200

    add requireAuth to upload page

--- server/router.js
+++ server/router.js
@@ -5,26 +5,32 @@ module.exports = function(app) {
   const passport = require('passport');
 
   const multer = require('multer');
-  const upload = multer({ dest: 'uploads/' });
+  var storage =   multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+      //callback(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+  var upload = multer({ storage : storage });
   const fs = require('fs');
 
   const requireAuth = passport.authenticate('jwt', { session: false });
   const requireSignin = passport.authenticate('local', { session: false });
 
 
-
-
   app.get('/', requireAuth, function(req, res) {
     res.send({ message: 'this is a secure path with a message from the API server' });
   });
 
+  app.use('./upload', requireAuth);
   app.post('/upload', upload.single('file'), function (req, res) {
     console.log(req.file);
     // res.send(req);
   });
-  // app.post('/upload', upload.single('file'), function (req, res, next) {
-  //   res.send({ responseText: req.file.path }); // You can send any response to the user here
-  // });
 
   app.post('/signin', requireSignin, Authentication.signin)
   app.post('/signup', Authentication.signup);
