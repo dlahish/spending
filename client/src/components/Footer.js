06859972815216dcea5893ec5f9c3dc763190792
Nadav Lachish <d_lahish@walla.com>
2016-07-02 21:07:53 +0200

    fix footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-      <div>Hello from footer</div>
+      <div>creater by Nadav Lachish</div>
 );
 
 export default Footer;
--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -94,6 +94,8 @@ const validate = formProps => {
 
   if (!formProps.email) {
     errors.email = 'Please enter an email';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formProps.email)) {
+      errors.email = 'Invalid email address'
   }
 
   if (!formProps.password) {
