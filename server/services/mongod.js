d7bfb923012c875d4708f644f0baa9e6a920f01c
Nadav Lachish <d_lahish@walla.com>
2016-07-15 00:59:13 +0200

    work on get total by month

--- server/services/mongod.js
+++ server/services/mongod.js
@@ -22,12 +22,114 @@ exports.getTotal = function(req, res) {
 
 function getMonthIncomeTotal(userId, year) {
   var yearIncomeTotal = [];
-  var yearIncomeExpenses = [];
-  for (var i=1; i=12; i++) {
-    var monthStart = moment(year/i/1);
-    // Data.find({ user: userId, date: { $gt: startDate, $lte: endDate } }, function(err, data){
-    // }
+  var yearExpensesTotal = [];
+  var monthStart;
+  var monthEnd;
+  var yearArray = [];
+  // monthStart = moment(1 + '/' + i + '/' + year, "DD/MM/YYYY");
+  console.log('MONTH START------------');
+  console.log(typeof year);
+
+  function asyncLoop(iterations, func, callback) {
+    var index = 1;
+    var done = false;
+    var loop = {
+        next: function() {
+            if (done) {
+                return;
+            }
+
+            if (index < iterations) {
+                index++;
+                func(loop);
+
+            } else {
+                done = true;
+                callback(yearArray);
+            }
+        },
+
+        iteration: function() {
+            return index - 1;
+        },
+
+        break: function() {
+            done = true;
+            callback();
+        }
+    };
+    loop.next();
+    return loop;
   }
+
+  function getMonth(year, j, callback) {
+    var nextYear = parseInt(year);
+    nextYear += 1;
+    nextYear = nextYear.toString();
+    yaer = year.toString();
+    console.log('NEXT YEAR -------');
+    console.log(nextYear);
+    console.log(typeof nextYear);
+    console.log('------ J ------');
+    console.log(j);
+    var jj = j + 1;
+    // if (j < 10) {
+    //     var preStart = '0' + 1 + '/' + '0' + j + '/' + year;
+    // } else {
+    //     var preStart = '0' + 1 + '/' + j + '/' + year;
+    // }
+
+
+    var monthStart = moment(1 + '/' + j + '/' + year, "DD/MM/YYYY");
+    if (j === 12) {
+        var monthEnd = moment(31 + '/12/' + year, "DD/MM/YYYY");
+    } else {
+        var monthEnd = moment(1 + '/' + jj + '/' + year, "DD/MM/YYYY");
+    }
+
+
+
+    Data.find({ user: userId, date: { $gt: monthStart, $lte: monthEnd } }, function(err, data){
+      if (err) { console.log(err); }
+      if (!data) { console.log('No data found'); }
+      callback(data);
+    });
+    // callback(preStart);
+  }
+
+  asyncLoop(
+    13,
+    function(loop) {
+      var j = loop.iteration();
+
+      var monthIncome = 0;
+      var monthExpenses = 0;
+      getMonth(2016, j, function(data){
+
+
+        console.log('DATA FROM  MONGO -----');
+        data.map((d) => {
+          if (!d.amount) {
+            yearArray.push([0,0]);
+          } else if (d.amount > 0) {
+              monthIncome += d.amount;
+              console.log(monthIncome);
+          } else {
+              monthExpenses += d.amount;
+          }
+        });
+        console.log('AFTER MAPPING ------');
+        yearArray.push({ income: monthIncome, expenses: monthExpenses });
+        console.log(yearArray);
+        console.log(j);
+        loop.next();
+      })},
+    function(yearArray){
+      console.log('cycle ended');
+      console.log(yearArray);
+    }
+  );
+
 };
 
 exports.getYearTotal = function(req, res) {
@@ -40,6 +142,8 @@ exports.getDataByDate = function(req, res) {
   const userId = req.user._id;
   const startDate = req.body.startDate;
   const endDate = req.body.endDate;
+
+  // getMonthIncomeTotal(userId, 2016);
 
   Data.find({ user: userId, date: { $gt: startDate, $lte: endDate } }, function(err, data){
     if (err) { console.log(err); }
@@ -56,6 +160,7 @@ exports.getDataByDate = function(req, res) {
       }
     });
     res.send({ data: data, searchTotalIncome: searchTotalIncome, searchTotalExpenses: searchTotalExpenses });
+    getMonthIncomeTotal(userId, 2016);
   });
 };
 
