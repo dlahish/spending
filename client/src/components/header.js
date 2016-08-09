59da881042e4bf91d6afc2dea87306bf39582a84
Nadav Lachish <d_lahish@walla.com>
2016-08-09 19:50:58 +0200

    work on new header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,228 +1,57 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
-import Signin from './auth/signin';
-import { browserHistory } from 'react-router';
-import * as actions from '../actions';
-import { withRouter } from 'react-router';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import TextField from 'material-ui/TextField';
-import FlatButton from 'material-ui/FlatButton';
-import { deepPurple300, grey50, red900 } from 'material-ui/styles/colors';
-import Popover from 'material-ui/Popover/Popover';
-import {Menu, MenuItem} from 'material-ui/Menu';
-import RaisedButton from 'material-ui/RaisedButton';
-import IconButton from 'material-ui/IconButton/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import FontIcon from 'material-ui/FontIcon';
-import UploadIcon from 'material-ui/svg-icons/file/file-upload';
-import EjectIcon from 'material-ui/svg-icons/action/eject';
-import ControlPoint from 'material-ui/svg-icons/image/control-point';
-import DescriptionIcon from 'material-ui/svg-icons/action/description';
+import React, { Component } from 'react'
 import Logo from '../../../media/logo.png';
 
 const styles = {
-  toolbartitle: {
-    cursor: 'pointer'
+  header: {
+    padding: '20px 40px',
+    height: '110px',
+    backgroundColor: 'white',
+    color: 'grey'
   },
-
-  toolbaruser: {
-    cursor: 'pointer',
-    fontWeight: '600',
-    // marginRight: '-15px',
-    lineHeight: '25px'
-  },
-
-  toolbar: {
-    backgroundColor: '#464646',
-    color: 'white',
-    // top: 0,
-    // position: 'fixed',
-    // width: '100%',
-    // zIndex: 1
-  },
-
-  flatbutton: {
-    marginRight: -15,
-    marginLeft: 10,
-    color: 'white'
-  },
-
-  textfield: {
-    marginRight: 5
-  },
-
   logo: {
-    width: '50px',
-    cursor: 'pointer'
+    position: 'relative',
+    height: '45px',
+    width: 'auto',
+    float: 'left',
+    bottom: '12px'
+  },
+  navUl: {
+    // display: 'inline-block',
+    // float: 'left',
+    // marginLeft: '11.25em',
+    textAlign: 'center',
+    // minWidth: '350px',
+    margin: '0 auto',
+    padding: 0
+  },
+  navLi: {
+    display: 'inline',
+    marginLeft: '50px',
+    fontWeight: '100',
+    letterSpacing: '2px',
+    fontSize: '20px'
+  },
+  hr: {
+    width: '90%',
+    textAlign: 'center',
+    margin: '5px auto',
+    display: 'inline-block'
   }
 }
 
-class Header extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      open: false,
-      anchorOrigin: {
-        horizontal: 'right',
-        vertical: 'bottom',
-      },
-      targetOrigin: {
-        horizontal: 'right',
-        vertical: 'top',
-      },
-    };
-  }
-
-  handleTouchTap = (event) => {
-    // This prevents ghost click.
-    event.preventDefault();
-    this.setState({
-      open: true,
-      anchorEl: event.currentTarget,
-    });
-  };
-
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  handleMenuLink() {
-    this.handleRequestClose();
-    browserHistory.push(this.value);
-  }
-
-  handleSignout() {
-    this.props.signoutUser();
-    browserHistory.push('/signout');
-  }
-
-  handleDashboard() {
-    this.handleRequestClose()
-    browserHistory.push('/dashboard');
-  }
-
-  handleDashboard() {
-    this.handleRequestClose()
-    browserHistory.push('/dashboard');
-  }
-
-  renderLinks() {
-    if (this.props.route == '/signinattempt') { return; }
-    if (this.props.authenticated) {
-      return (
-        <ToolbarGroup>
-          <FlatButton
-            style={styles.flatbutton}
-            labelStyle={{color: 'white'}}
-            label="Dashboard"
-            linkButton={true}
-            containerElement={<Link to="/dashboard"></Link>}
-          />
-          <ToolbarSeparator style={{ backgroundColor: 'white', width: '2px' }}/>
-          <FlatButton
-            style={styles.flatbutton}
-            labelStyle={{color: 'white'}}
-            label="Data"
-            linkButton={true}
-            containerElement={<Link to="/data"></Link>}
-          />
-          <ToolbarSeparator style={{ backgroundColor: 'white', width: '2px' }}/>
-          <ToolbarGroup onTouchTap={this.handleTouchTap} style={{ paddingLeft: '25px' }}>
-            <p style={styles.toolbaruser}>{this.props.user}</p>
-            <FontIcon
-              className="material-icons"
-              style={{ fontSize: '25px', cursor: 'pointer', color: 'white' }}
-            >arrow_drop_down
-            </FontIcon>
-            <Popover
-              open={this.state.open}
-              anchorEl={this.state.anchorEl}
-              anchorOrigin={this.state.anchorOrigin}
-              targetOrigin={this.state.targetOrigin}
-              onRequestClose={this.handleRequestClose}
-              style={{ marginTop: '0px' }}
-            >
-              <Menu>
-                {/*<MenuItem
-                  linkButton
-                  containerElement={<Link to="/dashboard" />}
-                  primaryText="Dashboard"
-                  onTouchTap={this.handleRequestClose.bind(this)}
-                />*/}
-                <MenuItem
-                  linkButton
-                  containerElement={<Link to="/upload" />}
-                  primaryText="Upload File"
-                  onTouchTap={this.handleRequestClose.bind(this)}
-                  leftIcon={<UploadIcon />}
-                />
-                <MenuItem
-                  linkButton
-                  containerElement={<Link to="/newrecord" />}
-                  primaryText="New Record"
-                  onTouchTap={this.handleRequestClose.bind(this)}
-                  leftIcon={<ControlPoint />}
-                />
-                <MenuItem
-                  linkButton
-                  containerElement={<Link to="/categories" />}
-                  primaryText="Categories"
-                  onTouchTap={this.handleRequestClose.bind(this)}
-                  leftIcon={<DescriptionIcon />}
-                />
-                <MenuItem
-                  primaryText="Sign out"
-                  onTouchTap={this.handleSignout.bind(this)}
-                  leftIcon={<EjectIcon />}
-                />
-              </Menu>
-          </Popover>
-          </ToolbarGroup>
-        </ToolbarGroup>
-      );
-    }
-
-    return (
-      <ToolbarGroup>
-        <Signin />
-      </ToolbarGroup>
-    );
-  }
-
-  handleTitleTouchTap() {
-    browserHistory.push('/');
-  }
-
+export default class Header extends Component {
   render() {
-    const imgPath = './spending_logo.png'
-    return(
-      <div>
-        <Toolbar style={styles.toolbar}>
-          {/*<ToolbarTitle
-            style={styles.toolbartitle}
-            text="Spending"
-            onTouchTap={this.handleTitleTouchTap}
-          />*/}
-          <img src={Logo} style={styles.logo} alt="spengin logo" onTouchTap={this.handleTitleTouchTap} />
-            {this.renderLinks()}
-        </Toolbar>
+    return (
+      <div style={styles.header}>
+        <img src={Logo} alt="spending logo" style={styles.logo}/>
+        <ul style={styles.navUl}>
+          <li style={styles.navLi}>Graphs</li>
+          <li style={styles.navLi}>Data Table</li>
+          <li style={styles.navLi}>Add Record</li>
+          <li style={styles.navLi}>Edit Categories</li>
+        </ul>
+        <hr style={styles.hr} />
       </div>
-    );
+    )
   }
 }
-
-function mapStateToProps(state, location) {
-  return {
-    authenticated: state.auth.authenticated,
-    errorMessage: state.auth.error,
-    route: state.auth.currentRoute,
-    user: state.auth.userEmail
-  };
-}
-
-export default withRouter(connect(mapStateToProps, actions)(Header));
